1671e5b847244b3c6ab7f8d71ffb1d75fcec3c10
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-02-09 22:21:33 +0800

    Create rowshrinkL20.m

---
+++ MMV-ADMM-L20/rowshrinkL20.m
@@ -0,0 +1,23 @@
+% Function that solves the L20-projection subproblem
+% Input: a given matrix C, with the known number of non-zero rows s
+% Output: the L20-projection matrix T
+% Copyright: Zekun Liu
+
+
+function T = rowshrinkL20(C,s)
+
+m = size(C,1);
+n = size(C,2);
+T = zeros(m,n);
+
+L2 = [];
+for i = 1:m
+    L2(i) = norm(C(i,:),2);
+end
+
+[~,index] = sort(L2,'descend');
+
+for i = 1:s
+    T(index(i),:) = C(index(i),:);
+end
+end
