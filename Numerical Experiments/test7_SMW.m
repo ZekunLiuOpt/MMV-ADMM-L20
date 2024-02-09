eecf74880d445ec3a689e19d907c5598f73cd6c4
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-02-10 00:42:56 +0800

    Update test7_SMW.m

--- Numerical Experiments/test7_SMW.m
+++ Numerical Experiments/test7_SMW.m
@@ -1,3 +1,7 @@
+% Experiments that comparing the improvement with SMW-formula to MMV-ADMM-L20
+% Copyright: Zekun Liu
+
+
 clear all
 clc
 
@@ -13,39 +17,28 @@ timesaver2 = [];
 RMSEsaver2 = [];
 
 T = 10;
-
 for i = 1:T
 
-    Index_K = randperm(N);%Sparse support set
-    
-    spar_arr = randn(1,K*J);%The value of sparse matrix to be recovered
-    
-    S = sparse(repelem(Index_K(1:K),J),repmat([1:1:J],1,K),spar_arr,N,J);%N*J sparse matrix to be recovered
-    
-    Phi = sqrt(1/M)*randn(M,N);%M*N unit-norm Gaussian random matrix
-    
-    Y = Phi*S;%Measurement value matrix
+    Index_K = randperm(N);
+    spar_arr = randn(1,K*J);
+    S = sparse(repelem(Index_K(1:K),J),repmat([1:1:J],1,K),spar_arr,N,J);
+    Phi = sqrt(1/M)*randn(M,N);
+    Y = Phi*S;
     
     tic
-    S1 = MMV_ADMM_L20(Y, Phi, K, 1);%S1 is the recovered estimation of S.
+    S1 = MMV_ADMM_L20(Y, Phi, K, 1);
     toc
 
     timesaver1 = [timesaver1,toc];
-
     RMSEsaver1 = [RMSEsaver1,RMSE(S1,S)];
 
     tic
-    S2 = MMV_ADMM_L20_SMW(Y, Phi, K, 1);%S1 is the recovered estimation of S.
+    S2 = MMV_ADMM_L20_SMW(Y, Phi, K, 1);
     toc
 
     timesaver2 = [timesaver2,toc];
-
     RMSEsaver2 = [RMSEsaver2,RMSE(S2,S)];
-
 end
-
-save test9
-load test9
 
 mean(RMSEsaver1)
 std(RMSEsaver1)
@@ -56,3 +49,6 @@ mean(timesaver1)
 std(timesaver1)
 mean(timesaver2)
 std(timesaver2)
+
+save test9
+load test9
