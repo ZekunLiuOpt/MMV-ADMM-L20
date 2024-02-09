0602aeb012d214982873e3377de42b84b5208816
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-02-09 23:05:06 +0800

    Create rowshrinkL21.m

---
+++ MMV-ADMM-L21/rowshrinkL21.m
@@ -0,0 +1,19 @@
+% Function that solves the L21-shrink subproblem argmin_{T} kaka*||T||_{2,1}+1/2*||T-C||^2
+% Input: a given matrix C, with the parameter kaka
+% Output: the L21-shrink solution matrix T
+% Copyright: Zekun Liu
+
+
+function T = rowshrinkL21(C,kaka)
+
+m = size(C,1);
+n = size(C,2);
+T = zeros(m,n);
+
+for i = 1:m
+    l = norm(C(i,:),2);
+    if l > kaka
+        T(i,:) = (l-kaka)/l*C(i,:);
+    end
+end
+end
