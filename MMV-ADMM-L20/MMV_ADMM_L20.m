945be274b990f989f663874abd9d25019ede578b
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-02-14 10:59:21 +0800

    Update MMV_ADMM_L20.m

--- MMV-ADMM-L20/MMV_ADMM_L20.m
+++ MMV-ADMM-L20/MMV_ADMM_L20.m
@@ -1,6 +1,6 @@
 % Our algorithm for solving the MMV problem based on L20-norm
 % Input: the measurement matrix Y, the sensing matrix Phi, the row-sparsity s (if one has the prior information; otherwise using the formula in the paper to calculate s first, and one can notice that 
-% spark(Phi)=rank(Phi)+1=M+1 holds with high probability), the penalty parameter rho>0
+% spark(Phi)=rank(Phi)+1=M+1 holds with high probability for Gaussian random matrix), the penalty parameter rho>0
 % Output: the reconstructed sparse matrix S
 % Copyright: Zekun Liu
 
