9cafd596bede25f9b8f8a80ce5b63142e2563ead
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-06-27 15:31:36 +0800

    Update RMSE.m

--- Numerical Experiments/RMSE.m
+++ Numerical Experiments/RMSE.m
@@ -1,7 +1,7 @@
 % Function that calculates the RMSE 
 % Input: a recovered matrix x1, a ground truth x2
 % Output: the RMSE of x1 to x2
-% Written by: Zekun Liu
+% Written by: Zekun Liu (02/03/2023)
 
 
 function y = RMSE(x1,x2)
