6d480f580882a40a794407024505574c3c9b1ff4
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-07-22 20:22:16 +0800

    Update RMSE.m

--- Numerical Experiments/RMSE.m
+++ Numerical Experiments/RMSE.m
@@ -2,12 +2,13 @@
 % Input: a recovered matrix x1, a ground truth x2
 % Output: the RMSE of x1 to x2
 % Written by: Zekun Liu (02/03/2023)
+% Latest Revision: 22/07/2024
 
 
-function y = RMSE(x1,x2)
+function y = RMSE(x1, x2)
 
-N = size(x1,1);
-J = size(x1,2);
+N = size(x1, 1);
+J = size(x1, 2);
 
-y = norm(x1-x2,'fro')/sqrt(N*J);
+y = norm(x1 - x2, 'fro') / sqrt(N * J);
 end
