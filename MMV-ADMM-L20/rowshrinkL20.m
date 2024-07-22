db1546ae714e2eda954c9cba63cd3d7f0b48a830
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-07-22 20:16:46 +0800

    Update rowshrinkL20.m

--- MMV-ADMM-L20/rowshrinkL20.m
+++ MMV-ADMM-L20/rowshrinkL20.m
@@ -2,22 +2,26 @@
 % Input: a given matrix C, with the known number of non-zero rows s
 % Output: the L20-projection matrix T
 % Written by: Zekun Liu (11/02/2023)
+% Reference:
+% [1] Z. Liu and S. Yu. Alternating Direction Method of Multipliers Based on $\ell_{2,0}$-Norm for Multiple Measurement Vector Problem.
+% IEEE Transactions on Signal Processing, vol. 71, pp. 3490-3501, 2023.
+% Latest Revision: 22/07/2024
 
 
-function T = rowshrinkL20(C,s)
+function T = rowshrinkL20(C, s)
 
-m = size(C,1);
-n = size(C,2);
-T = zeros(m,n);
+m = size(C, 1);
+n = size(C, 2);
+T = zeros(m, n);
 
 L2 = [];
 for i = 1:m
-    L2(i) = norm(C(i,:),2);
+    L2(i) = norm(C(i, :), 2);
 end
 
-[~,index] = sort(L2,'descend');
+[~, index] = sort(L2, 'descend');
 
 for i = 1:s
-    T(index(i),:) = C(index(i),:);
+    T(index(i), :) = C(index(i), :);
 end
 end
