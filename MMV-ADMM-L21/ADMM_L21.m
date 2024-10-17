5d977b83b2ff32f27052c60a4cdd2955ab0c21a9
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-10-17 20:03:24 +0800

    Update ADMM_L21.m

--- MMV-ADMM-L21/ADMM_L21.m
+++ MMV-ADMM-L21/ADMM_L21.m
@@ -1,11 +1,22 @@
 % Existed algorithm for solving the MMV problem based on L21-norm
-% Input: the measurement matrix X, the sensing matrix A, the parameter gamma > 0, rho > 0
-% Output: the reconstructed sparse matrix B
-% Written by: Zekun Liu (10/02/2023)
+%
+% Input: 
+%         X: the measurement matrix
+%         A: the sensing matrix
+%     gamma: the regularization paramter (> 0)
+%       rho: the penalty parameter (> 0)
+%
+% Output: 
+%         B: the reconstructed sparse matrix
+%
+% Written by Zekun Liu, 10/02/2023
+%
 % Reference:
-% [1] Q. Qu, N. M. Nasrabadi, and T. D. Tran. Abundance estimation for bilinear mixture models via joint sparse 
-%     and low-rank representation. IEEE Trans. Geosci. Remote Sens., vol. 52, no. 7, pp. 4404–4423, Jul.2014.
-% Latest Revision: 23/07/2024
+% [1] Q. Qu, N. M. Nasrabadi, and T. D. Tran. 
+%     Abundance estimation for bilinear mixture models via joint sparse and low-rank representation.
+%     IEEE Trans. Geosci. Remote Sens., vol. 52, no. 7, pp. 4404–4423, Jul.2014.
+%
+% Latest Revision: 17/10/2024
 
 
 function B = ADMM_L21(X, A, gamma, rho)
@@ -17,7 +28,7 @@ L = zeros(r, c);  % Initialize Lagragian to be nothing (seems to work well)
 maxIter = 1000;   % Set the maximum number of iterations (make really big to ensure convergence)
 I = speye(r);     % Set the sparse identity matrix
 C = randn(r, c);  % Initialize C randomly
-epsilon1 = 1e-6;  %stopping criteria 
+epsilon1 = 1e-6;  % Stopping criteria 
 epsilon2 = 1e-6;
 
 invA = inv(A' * A + rho * I);
@@ -28,9 +39,11 @@ for n = 1:maxIter
     C = rowshrinkL21(B + (L / rho), gamma / rho); 
     L = L + rho * (B - C);  
 
+    % For comparisons, one can also only check if rd < epsilon2
     rp = norm(B - C, 'fro');
     rd = norm(Cold - C, 'fro');
-    if (rp < epsilon1) && (rd < epsilon2)  % For comparisons, one can also only check if rd < epsilon2.
+    if (rp < epsilon1) && (rd < epsilon2)  
+    
         break;
     end
 end
