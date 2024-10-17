2403fff9a8daedb713f1b9b88b42398d160e060a
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-10-17 19:59:26 +0800

    Update sniht.m

--- sniht.m
+++ sniht.m
@@ -1,12 +1,22 @@
 % Existed algorithm for solving the MMV problem based on IHT
-% Input: the measurement matrix Y, the sensing matrix A, the row-sparsity k, printitn := print iteration number (modulo)
-% Output: the reconstructed sparse matrix Xnew
-% Written by E. Ollila (15/10/2014): 
-% https://github.com/AmmarMian/huber_mm_framework/blob/9b2f84772e21807c6ea6fb72c18d33c99b51b2e6/python/notebooks/matlab/sniht.m#L4
+%
+% Input: 
+%         Y: the measurement matrix Y
+%         A: the sensing matrix
+%         k: the row-sparsity
+%
+% Output: 
+%      Xnew: the reconstructed sparse matrix
+%
+% Written by E. Ollila, 15/10/2014 (https://github.com/AmmarMian/huber_mm_framework/
+% blob/9b2f84772e21807c6ea6fb72c18d33c99b51b2e6/python/notebooks/matlab/sniht.m#L4)
+%
 % Reference: 
-% [1] J. D. Blanchard, M. Cermak, D. Hanle, and Y. Jing. Greedy algorithms for joint sparse recovery. 
+% [1] J. D. Blanchard, M. Cermak, D. Hanle, and Y. Jing.
+%     Greedy algorithms for joint sparse recovery. 
 %     IEEE Trans. Signal Process., vol. 62, no. 7, pp. 1694–1704, Apr. 2014.
-% Latest Revision: 22/07/2024
+%
+% Latest Revision: 17/10/2024
 
 
 function Xnew = sniht(Y, A, k, X0supp, printitn) 
@@ -17,23 +27,23 @@ function Xnew = sniht(Y, A, k, X0supp, printitn)
 if nargin < 5, printitn = 0; end
 if nargin < 4, X0supp = []; end
 
-%%-- initial approximation is the zero matrix 
+% initial approximation is the zero matrix 
 X0  = zeros(n, q); 
 
 ARRAYSIM =  false;
 
-%%- DetectSupport
-if isempty(X0supp),
+% DetectSupport
+if isempty(X0supp)
     R = A' * Y;
     [~, indx] = sort(sum(R .* conj(R), 2), 'descend');
     X0supp = indx(1:k);
     
-    if ARRAYSIM,
-        if abs(indx(2) - indx(1)) == 1, 
+    if ARRAYSIM
+        if abs(indx(2) - indx(1)) == 1 
              X0supp(2) = indx(3); 
         end 
      
-        if  abs(X0supp(2) - indx(1)) == 1,
+        if  abs(X0supp(2) - indx(1)) == 1
             X0supp(2) = indx(4); 
         end
         % X0supp = sort(X0supp);
@@ -43,30 +53,30 @@ end
 
 ITERMAX = 1000;
 % cc = 0.02;
-objold = 1e12; % a big number
+objold = 1e12;  % a big number
 failure = 0;
 
 for iter = 1:ITERMAX
     
-    %%-- Compute the negative gradient 
+    % Compute the negative gradient 
     R = Y - A * X0;
     G = A' * R; 
 
-    %%-- stepsize
+    % stepsize
     mu = norm(G(X0supp, :), 'fro') / norm(A(:, X0supp) * G(X0supp, :), 'fro');
     mu = mu^2;
 
-    %%-- Next proposal for b 
+    % Next proposal for b 
     X1 = X0 + mu * G;
-    %%-- Detect Support
+    % Detect Support
     [~, indx] = sort(sum(X1 .* conj(X1), 2), 'descend');
     X1supp = indx(1:k);
     
-    %%-- Threshold
+    % Threshold
     Xnew = zeros(n, q);
     Xnew(X1supp, :) = X1(X1supp, :);
 
-    %%-- Stopping criteria          
+    % Stopping criteria          
     crit = norm(Xnew - X0, 'fro')^2 / norm(Xnew, 'fro')^2;
     % crit = norm(bnew - b0)^2;
        
@@ -90,6 +100,6 @@ for iter = 1:ITERMAX
 end
 
 % fprintf('NORIHT terminating at iter = %d crit = %f\n', iter, crit)  
-if iter == ITERMAX, failure = 1; end;
+if iter == ITERMAX, failure = 1; end
 X1supp = sort(X1supp);
 end
