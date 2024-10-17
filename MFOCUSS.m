fa4f693c7ebc3ab2a91f50a8e01bbf01a9747e18
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-10-17 20:01:40 +0800

    Update MFOCUSS.m

--- MFOCUSS.m
+++ MFOCUSS.m
@@ -1,13 +1,27 @@
 % Existed algorithm for solving the MMV problem based on FOCUSS
-% Input: the measurement matrix Y, the sensing matrix Phi, the regularization parameter lambda > 0, generally it is close to the noise variance. 
-% In the noiseless cases, simply setting lambda = 1e-10 leads to good performance.
-% Output: the reconstructed sparse matrix X, the indexes of nonzero gamma_ind, the final values of all the gamma_ind (including zeros) gamma_est,
-% the number of iterations used count
-% Written by Zhilin Zhang, David Wipf (05/12/2011): https://github.com/JustinGirard/MatlabCISL/blob/master/MFOCUSS.m
+%
+% Input: 
+%         Y: the measurement matrix
+%       Phi: the sensing matrix Phi
+%    lambda: the regularization parameter (> 0)
+%            generally it is close to the noise variance 
+%            in the noiseless cases, simply setting lambda = 1e-10 leads to good performance
+%
+% Output: 
+%         X: the reconstructed sparse matrix
+% gamma_ind: the indexes of nonzero
+% gamma_est: the final values of all the gamma_ind (including zeros)
+%     count: the number of iterations used 
+%
+% Written by Zhilin Zhang, David Wipf, 05/12/2011
+% (https://github.com/JustinGirard/MatlabCISL/blob/master/MFOCUSS.m)
+%
 % Reference: 
-% [1] S. F. Cotter, B. D. Rao, K. Engan, and K. Kreutz-Delgado. Sparse solutions to linear inverse problems with multiple measurement vectors.
+% [1] S. F. Cotter, B. D. Rao, K. Engan, and K. Kreutz-Delgado. 
+%     Sparse solutions to linear inverse problems with multiple measurement vectors.
 %     IEEE Trans. Signal Process., vol. 53, no. 7, pp. 2477–2488, 2005.
-% Latest Revision: 22/07/2024
+%
+% Latest Revision: 17/10/2024
 
 
 function [X, gamma_ind, gamma_est, count] = MFOCUSS(Phi, Y, lambda, varargin)
@@ -32,11 +46,11 @@ function [X, gamma_ind, gamma_est, count] = MFOCUSS(Phi, Y, lambda, varargin)
 [N L] = size(Y);  
 
 % Default Control Parameters 
-PRUNE_GAMMA = 1e-4;        % threshold for prunning small gamma_i
-p           = 0.8;         % p-norm
-EPSILON     = 1e-6;        % threshold for stopping iteration. 
-MAX_ITERS   = 1000;        % maximum iterations. 
-PRINT       = 0;           % not show progress information
+PRUNE_GAMMA = 1e-4;  % threshold for prunning small gamma_i
+p           = 0.8;   % p-norm
+EPSILON     = 1e-6;  % threshold for stopping iteration. 
+MAX_ITERS   = 1000;  % maximum iterations. 
+PRINT       = 0;     % not show progress information
 
 % get input argument values
 if(mod(length(varargin), 2) == 1)
@@ -63,11 +77,11 @@ end
 if (PRINT) fprintf('\nRunning M-FOCUSS for the MMV Problem...\n'); end
 
 % Initializations 
-gamma = ones(M, 1);        % initialization of gamma_i
-keep_list = [1:M]';        % record the index of nonzero gamma_i
-m = length(keep_list);     % number of nonzero gamma_i
-mu = zeros(M, L);          % initialization of the solution matrix
-count = 0;                 % record iterations
+gamma = ones(M, 1);     % initialization of gamma_i
+keep_list = [1:M]';     % record the index of nonzero gamma_i
+m = length(keep_list);  % number of nonzero gamma_i
+mu = zeros(M, L);       % initialization of the solution matrix
+count = 0;              % record iterations
 
 while (1)
 
@@ -75,12 +89,12 @@ while (1)
     if (min(gamma) < PRUNE_GAMMA)
         index = find(gamma > PRUNE_GAMMA);
         gamma = gamma(index);   
-        Phi = Phi(:, index);            % corresponding columns in Phi
+        Phi = Phi(:, index);  % corresponding columns in Phi
         keep_list = keep_list(index);
         m = length(gamma);
 
-        if (m == 0)   break;  end;
-    end;
+        if (m == 0)   break;  end
+    end
 
     % ====== Compute new weights ======
     G = repmat(sqrt(gamma)', N, 1);
@@ -89,7 +103,7 @@ while (1)
 
     [d1, d2] = size(S);
     if (d1 > 1)     diag_S = diag(S);
-    else            diag_S = S(1);      end;
+    else            diag_S = S(1);      end
 
     U_scaled = U(:, 1:min(N, m)) .* repmat((diag_S ./ (diag_S.^2 + sqrt(lambda) + 1e-16))', N, 1);
     Xi = G' .* (V * U_scaled');
@@ -105,15 +119,15 @@ while (1)
     % ========= Check stopping conditions, etc. ========= 
     count = count + 1;
     if (PRINT) disp(['iters: ', num2str(count), '   num coeffs: ', num2str(m), ...
-            '   gamma change: ', num2str(max(abs(gamma - gamma_old)))]); end;
-    if (count >= MAX_ITERS) break;  end;
+            '   gamma change: ', num2str(max(abs(gamma - gamma_old)))]); end
+    if (count >= MAX_ITERS) break;  end
 
     if (size(mu) == size(mu_old))
         dmu = max(max(abs(mu_old - mu)));
-        if (dmu < EPSILON)  break;  end;
-    end;
+        if (dmu < EPSILON)  break;  end
+    end
 
-end;
+end
 
 gamma_ind = sort(keep_list);
 gamma_est = zeros(M, 1);
